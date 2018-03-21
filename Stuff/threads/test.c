b6f528739c82867c5a6aab2b7ea6f9a8d62edbcc
franspaco <franspaco@gmail.com>
2018-03-21 13:14:17 -0600

    blackjack

--- Stuff/threads/test.c
+++ Stuff/threads/test.c
@@ -11,5 +11,45 @@ void* startThread(void*);
 
 
 int main(){
+    pthread_t tids[NUM_THREADS];
+    int status;
+    long result;
+
+    for(long i = 0; i < NUM_THREADS; i++){
+        status = pthread_create(&tids[i], NULL, startThread, (void*) i);
+        printf("Thread %ld created with ID: %ld\n", i, tids[i]);
+        if (status != 0)
+        {
+            perror("ERROR: pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
     
+    // Wait for all threads to finish
+    for (int i=0; i<NUM_THREADS; i++)
+    {
+        status = pthread_join(tids[i], (void *)&result);
+        if (status != 0)
+        {
+            perror("ERROR: pthread_join");
+            exit(EXIT_FAILURE);
+        }
+        printf("Thread %d returned value %ld\n", i, (long)result);
+    }
+    
+    // Wait for all other attached threads to finish
+    pthread_exit(NULL);
+    
+    return 0;
+}
+
+void * startThread(void * arg)
+{
+    long index = (long) arg;
+    
+    printf("\tThis is thread %ld\n", index);
+    sleep(1);
+    printf("\tThread %ld is about to finish\n", index);
+    
+    pthread_exit( (void *) (index * 2) );
 }
